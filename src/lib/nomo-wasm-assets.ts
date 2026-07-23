ee5301b42ba50a00e10e228a4a676f8108cdcb6d
Fynn Tang <fzpying@gmail.com>
2026-07-23 23:02:05 +0800

    feat: ship std fmt in playground

--- src/lib/nomo-wasm-assets.ts
+++ src/lib/nomo-wasm-assets.ts
@@ -1,5 +1,5 @@
 export const NOMO_WASM_SHA256 =
-  "1ccd7db5b0673580646d647b6815d3f8204b0f01a026718c8bf1048852f32a6e";
+  "3c550d54f450007c8275069c74187bccd22d4433f5c926a3a3d6003a8eb87ea9";
 
 export function nomoWasmAssetUrl(
   sha256 = NOMO_WASM_SHA256,
